0d0ef11e37b316df43b71842f9a2ce2362f28c58
lynxzpoi <zakiyudhistira@gmail.com>
2024-01-21 01:23:14 +0700

    update button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,36 +4,54 @@ import Link from "next/link"
 
 
 const Page = () => {
-return (
-    <div className="container mx-6 py-8">
-        <section className="text-start">
-            <h1 className=" text-4xl font-bold mb-4 pt-10">WARKOP CAMP</h1>
-            {/* <p className="">Tujuan kami adalah memberikan pelayanan terbaik untuk setiap customer</p> */}
-        </section>
+    return (
+        <div className="container mx-6 py-8">
+            <section className="text-start">
+                <h1 className=" text-4xl font-bold mb-4 pt-10">WARKOP CAMP</h1>
+                {/* <p className="">Tujuan kami adalah memberikan pelayanan terbaik untuk setiap customer</p> */}
+            </section>
 
-        <section className="mt-8">
-            <h2 className="text-2xl text-center font-semibold mb-6 mt-10 pt-10">Please Select The Panel Below</h2>
-            {/* Button Section Start */}
-            <div className="grid grid-cols-2 gap-8 px-4"> 
-                    <Link href={`/persediaan`} type="button" className="bg-neutral-700/40 hover:bg-neutral-700 transition-all duration-300 focus:outline-none focus:ring focus:ring-slate-700/90 p-4 rounded-md border border-neutral-600/30">
-                        <div className="flex flex-col items-center">
-                            <ClipboardText size={100} color="#737373"/>
-                            <p className="text-xl font-semibold mb-2">PERSEDIAAN</p>
+            <section className="mt-8">
+                <h2 className="text-2xl text-center font-semibold mb-6 mt-10 pt-10">Please Select The Panel Below</h2>
+                {/* Button Section Start */}
+                <div className="grid grid-cols-2 gap-8 px-4">
+                    <Link href={`/persediaan`} type="button" className="relative group bg-neutral-700/40 p-4
+                      before:absolute 
+                      before:inset-0 
+                      before:bg-neutral-700
+                      before:scale-x-0 
+                      before:origin-right
+                      before:transition
+                      before:duration-300
+                      hover:before:scale-x-100
+                      hover:before:origin-left">
+                        <span><div className="relative flex flex-col items-center">
+                            <ClipboardText size={100} color="#737373" />
+                            <p className="text-xl font-semibold pt-2">PERSEDIAAN</p>
                             <h2 className="">Cek Menu Yang Masih Tersedia Untuk Kita Jual</h2>
-                        </div>
-                        </Link>
-                    <Link href={`/pembelian`} type="button" className="bg-neutral-700/40 hover:bg-neutral-700 transition-all duration-300 focus:outline-none focus:ring focus:ring-slate-700/90 p-4 rounded-md border border-neutral-600/30">
-                        <div className="flex flex-col items-center">
-                            <ShoppingCartSimple size={100} color="#737373"/>
-                            <p className=" text-xl font-semibold mb-2">PEMBELIAN</p>
-                            <h2 className="">Cek Menu Yang Masih Tersedia Untuk Kita Jual</h2>
-                        </div>
+                        </div></span>
                     </Link>
-            </div>
-            {/* Button Section End */}
-        </section>
-    </div>
-)
+                    <Link href={`/pembelian`} type="button" className="relative group bg-neutral-700/40 p-4
+                      before:absolute 
+                      before:inset-0 
+                      before:bg-neutral-700
+                      before:scale-x-0 
+                      before:origin-right
+                      before:transition
+                      before:duration-300
+                      hover:before:scale-x-100
+                      hover:before:origin-left">
+                        <span><div className="relative flex flex-col items-center">
+                            <ShoppingCartSimple size={100} color="#737373" />
+                            <p className=" text-xl font-semibold pt-2">PEMBELIAN</p>
+                            <h2 className="">Cek Menu Yang Masih Tersedia Untuk Kita Jual</h2>
+                        </div></span>
+                    </Link>
+                </div>
+                {/* Button Section End */}
+            </section>
+        </div>
+    )
 }
 
 export default Page
--- src/app/persediaan/page.jsx
+++ src/app/persediaan/page.jsx
@@ -2,22 +2,20 @@ import Link from "next/link"
 
 const Page = () => {
     return (
-        
-            <div className="container mx-auto py-8">
-                <section className="text-center">
-                    <h1 className="text-slate-100 text-4xl font-bold mb-4">WARKOP CAMP</h1>
-                    <p className="text-slate-100">Tujuan kami adalah memberikan pelayanan terbaik untuk setiap customer</p>
-                </section>
+        <div className="container mx-6 py-8">
+        <section className="text-start">
+            <p className=" text-4xl font-bold mb-4 pt-10">WARKOP CAMP</p>
+            {/* <h1 className="text-3xl">Pembelian</h1> */}
+        </section>
+      
 
-                <section>
-                <Link href={`/#`} type="button" className="bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring focus:ring-blue-300 p-4 rounded-3xl">
-                        <p className="text-xl font-semibold mb-2 text-slate-100">BACK</p>
-                        <h2 className=" text-slate-100">Kembali Ke Menu Utama</h2>
+            <section> 
+                    <Link href={`/`} type="button" className="bg-neutral-700/40 hover:bg-neutral-700 transition-all duration-300 focus:outline-none focus:ring focus:ring-slate-700/90 p-4 rounded-md border border-neutral-600/30">
+                        <p className="text-xl font-semibold mb-2">BACK</p>
+                        <h2 className="">Kembali Ke Menu Utama</h2>
                     </Link>
                 </section>
-
-            </div>
-
+        </div>
     )
     }
         
