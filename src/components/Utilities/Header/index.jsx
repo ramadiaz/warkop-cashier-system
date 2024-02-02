4e48e07d6278d9f89fe5f6de81472602a4f217a2
Rama Diaz <ramadiaz221@gmail.com>
2024-02-03 03:21:24 +0700

    Header update

--- src/components/Utilities/Header/index.jsx
+++ src/components/Utilities/Header/index.jsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+
 const Header = ({ title }) => {
   return (
     <div className="w-full border-b border-neutral-600/50 flex justify-between items-center">
       <h1 className="py-4 px-4 text-xl font-semibold">{title}</h1>
-      <div className="text-right">
-        <h3 className="px-4 text-lg font-semibold">Wyvern</h3>
-        <h4 className="px-4 text-xs ">Login as Cashier</h4>
+      <div className="flex flex-row">
+        <div className="text-right">
+          <h3 className="px-4 text-lg font-semibold">Wyvern</h3>
+          <h4 className="px-4 text-xs ">Login as Cashier</h4>
+        </div>
+        <Image src={"/img/avatar.png"} alt="Avatar" width={400} height={400} className="w-12 rounded-full mr-4 scale-90"/>
       </div>
     </div>
   );
