d152eb3efed31ef1b108ee96ee1a5779e59d7b46
Rama Diaz <ramadiaz221@gmail.com>
2024-02-24 03:53:01 +0700

    Delete unnecessary API router

--- src/app/api/v1/getInvoice/route.js
+++
@@ -1,8 +0,0 @@
-import prisma from "@/app/libs/prisma";
-
-export async function GET(request) {
-
-    const invoice = await prisma.Invoice.findMany()
-
-    return Response.json({status: 200, body: invoice})
-}
