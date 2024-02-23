ad6cd2cd9dd4ff917bb870f6b850d12ce623a174
Rama Diaz <ramadiaz221@gmail.com>
2024-02-24 04:26:26 +0700

    Minor update

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,11 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // matcher: [
-  //   "/",
-  //   "/storage",
-  //   "/transaction",
-  //   "/transactions-history",
-  //   "/settings",
-  // ],
+  matcher: [
+    // "/",
+    // "/storage",
+    // "/transaction",
+    // "/transactions-history",
+    // "/settings",
+  ],
 };
