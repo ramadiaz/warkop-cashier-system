a03e0b2c01b19f8819fe58d0eb54d2c2a12ab520
Rama Diaz <ramadiaz221@gmail.com>
2024-02-24 03:10:14 +0700

    Next configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      prerenderExclude: [
+        /^\/api\/.*/,
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
