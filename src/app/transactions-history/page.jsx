871d14ac0ccd7339ecaa6534dac2ae3eda5da154
Rama Diaz <ramadiaz221@gmail.com>
2024-02-28 01:10:00 +0700

    Transaction history page update

--- src/app/transactions-history/page.jsx
+++ src/app/transactions-history/page.jsx
@@ -30,49 +30,85 @@ const Page = () => {
   }, []);
 
   return (
-    <div className="transaction-history">
+    <div className="transaction-history overflow-visible">
       <Header title={"Transactions History"} />
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="flex flex-col">
+        <div className="flex flex-col overflow-scroll h-[880px]">
           <table>
             <thead>
-              <tr>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold py-2">id</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">date</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">items</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">quantity</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">totalAmount</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">cash</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">change</td>
-                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">cashier</td>
+              <tr className="divide-x divide-neutral-600/50 border-b border-neutral-600/50">
+                <td className="whitespace-nowrap font-semibold py-2 px-4">
+                  id
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  date
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  items
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  quantity
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  totalAmount
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  cash
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  change
+                </td>
+                <td className="whitespace-nowrap font-semibold px-4 py-2">
+                  cashier
+                </td>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="divide-y divide-neutral-600/50">
               {transactions?.map((item, index) => {
                 return (
-                  <tr key={index}>
-                    <td className="whitespace-nowrap border border-neutral-600/50 py-2 text">{item.id}</td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.createdAt}</td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">
+                  <tr key={index} className="divide-x divide-neutral-600/50">
+                    <td className="whitespace-nowrap py-2 px-4">
+                      {item.id}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 ">
+                      {item.createdAt}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 ">
                       <ul>
                         {item.menus.map((menu, index) => {
-                          return <li key={index} className="">{menu.name}</li>;
+                          return (
+                            <li key={index} className="">
+                              {menu.name}
+                            </li>
+                          );
                         })}
                       </ul>
                     </td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">
+                    <td className="whitespace-nowrap px-4 py-2 ">
                       <ul>
                         {item.menus.map((menu, index) => {
-                          return <li key={index} className="">x{menu.quantity}</li>;
+                          return (
+                            <li key={index} className="">
+                              x{menu.quantity}
+                            </li>
+                          );
                         })}
                       </ul>
                     </td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.totalAmount}</td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.cash}</td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.change}</td>
-                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.cashier.name}</td>
+                    <td className="whitespace-nowrap px-4 py-2 ">
+                      Rp.{item.totalAmount.toLocaleString()}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 ">
+                      Rp.{item.cash.toLocaleString()}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 ">
+                      Rp.{item.change.toLocaleString()}
+                    </td>
+                    <td className="whitespace-nowrap px-4 py-2 ">
+                      {item.cashier.name}
+                    </td>
                   </tr>
                 );
               })}
