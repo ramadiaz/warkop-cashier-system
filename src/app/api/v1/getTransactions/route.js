6669591a1ae6b76f74cec540be35c0411ae43d7f
Rama Diaz <ramadiaz221@gmail.com>
2024-02-27 23:54:25 +0700

    Transactions history page update

--- src/app/api/v1/getTransactions/route.js
+++ src/app/api/v1/getTransactions/route.js
@@ -2,7 +2,19 @@ import prisma from "@/app/libs/prisma";
 
 export async function GET(request) {
 
-    const transactions = await prisma.Transaction.findMany()
+    const transactions = await prisma.Transaction.findMany(
+        {
+            include: {
+                menus: true,
+                cashier: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            }
+        }
+    )
 
     return Response.json({status: 200, body: transactions})
 }
--- src/app/transactions-history/page.jsx
+++ src/app/transactions-history/page.jsx
@@ -3,40 +3,83 @@
 import Header from "@/components/Utilities/Header";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import Loading from "../loading";
 
 const Page = () => {
-  const session = useSession();
-  const [cashierData, setCashierData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async () => {
+    setIsLoading(true);
     try {
-      const res = await fetch(
-        `/api/v1/getUserInfo/${session?.data?.user?.email}`
-      );
+      const response = await fetch("/api/v1/getTransactions");
 
-      if (res.ok) {
-        const data = await res.json();
-        setCashierData(data);
+      if (response.ok) {
+        const data = await response.json();
+        setTransactions(data.body);
+        console.log(data.body);
       }
-
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
     }
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  console.log(cashierData);
-
   return (
-    <div>
-      <Header title={`Transaction History`} />
-      {isLoading ? "Loading" : <h3>{cashierData.body?.name}</h3>}
+    <div className="transaction-history">
+      <Header title={"Transactions History"} />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="flex flex-col">
+          <table>
+            <thead>
+              <tr>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold py-2">id</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">date</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">items</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">quantity</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">totalAmount</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">cash</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">change</td>
+                <td className="whitespace-nowrap border border-neutral-600/50 font-semibold px-4 py-2">cashier</td>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions?.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="whitespace-nowrap border border-neutral-600/50 py-2 text">{item.id}</td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.createdAt}</td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">
+                      <ul>
+                        {item.menus.map((menu, index) => {
+                          return <li key={index} className="">{menu.name}</li>;
+                        })}
+                      </ul>
+                    </td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">
+                      <ul>
+                        {item.menus.map((menu, index) => {
+                          return <li key={index} className="">x{menu.quantity}</li>;
+                        })}
+                      </ul>
+                    </td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.totalAmount}</td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.cash}</td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.change}</td>
+                    <td className="whitespace-nowrap border border-neutral-600/50 px-4 py-2 text">{item.cashier.name}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
