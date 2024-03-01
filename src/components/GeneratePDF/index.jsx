709fed90500c823190eb19ffb6904f2fa8e042c8
Rama Diaz <ramadiaz221@gmail.com>
2024-03-01 18:12:29 +0700

    Generate PDF Component initialization

---
+++ src/components/GeneratePDF/index.jsx
@@ -0,0 +1,43 @@
+'use client'
+import html2canvas from "html2canvas"
+import jsPDF from "jspdf"
+import { useRef } from "react"
+
+const GeneratePDF = () => {
+    const pdRef = useRef()
+
+    const downloadPDF = () => {
+        const input = pdRef.current
+        html2canvas(input).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png')
+            const pdf = new jsPDF('p', 'mm', 'a4', true)
+            const pdfWidth = pdf.internal.pageSize.getWidth()
+            const pdfHeight = pdf.internal.pageSize.getHeight()
+            const imgWidth = canvas.width
+            const imgHeight = canvas.height
+            const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight)
+            const imgX = (pdfWidth - imgWidth * ratio) / 2
+            const imgY = 30
+            pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio)
+            pdf.save('invoice.pdf')
+        })
+    }
+
+
+    return (
+        <>
+            <div ref={pdRef}>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <h2>hahahahahahaha</h2>
+            </div>
+            <button onClick={downloadPDF}>
+                Download PDF
+            </button>
+        </>
+    )
+}
+
+export default GeneratePDF
